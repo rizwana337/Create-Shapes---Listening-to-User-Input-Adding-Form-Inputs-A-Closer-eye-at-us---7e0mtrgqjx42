0890ba0619e0cfee2d84ff5943cfe9761bf3a85f
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-01 15:48:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,39 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
+const [selectedShape, setSelectedShape] = useState("square");
+const [shapes, setShapes] = useState([]);
 
-  return (
-    <div id="main">
-      <div id="shape-creator">
-        
+const handleSelectChange = (e) => {
+setSelectedShape(e.target.value);
+};
 
-      </div>
-      <div id="shapes-holder">
-      </div>
-    </div>
-  )
-}
+const handleAddShape = () => {
+const newShape = selectedShape === "square" ? (
+<div className="square"></div>
+) : (
+<div className="circle"></div>
+);
+setShapes([...shapes, newShape]);
+};
 
+return (
+<div id="main">
+<div id="shape-creator">
+<select value={selectedShape} onChange={handleSelectChange}>
+<option value="square">Square</option>
+<option value="circle">Circle</option>
+</select>
+<button onClick={handleAddShape}>Add shape</button>
+</div>
+<div id="shapes-holder">
+{shapes.map((shape, index) => (
+<div key={index}>{shape}</div>
+))}
+</div>
+</div>
+);
+};
 
 export default App;
